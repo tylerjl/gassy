6f5c75af09bcd65eb3fb76e2abb29687f12172ad
Tyler Langlois <tjl@byu.net>
2019-01-16 00:46:04 -0700

    It basically works

--- executable/Main.hs
+++ executable/Main.hs
@@ -1,27 +1,71 @@
 module Main where
 
+import ClassyPrelude hiding (for)
+
+import Data.Aeson
+import qualified Data.Vector as V
+import Database.V5.Bloodhound hiding (name)
+import Network.HTTP.Client (defaultManagerSettings)
+import Network.Wai.Middleware.Static
 import Web.Spock
 import Web.Spock.Config
 
-import Control.Monad.Trans
-import Data.IORef
-import Data.Monoid
-import qualified Data.Text as T
+import Text.Blaze.Html5 ((!))
+import qualified Text.Blaze.Html5 as H
+import Text.Blaze.Html5.Attributes hiding (title)
 
-data MySession = EmptySession
-data MyAppState = DummyAppState (IORef Int)
+import qualified Text.Blaze.Html.Renderer.Text as R
+
+application :: H.Html
+application = "Gassy"
+
+es :: Server
+es = Server "http://elasticsearch.service.consul:9200"
 
 main :: IO ()
-main =
-    do ref <- newIORef 0
-       spockCfg <- defaultSpockCfg EmptySession PCNoDatabase (DummyAppState ref)
-       runSpock 8080 (spock spockCfg app)
+main = do
+  spockCfg <- defaultSpockCfg () PCNoDatabase ()
+  runSpock 8090 (spock spockCfg app)
 
-app :: SpockM () MySession MyAppState ()
-app =
-    do get root $
-           text "Hello World!"
-       get ("hello" <//> var) $ \name ->
-           do (DummyAppState ref) <- getState
-              visitorNumber <- liftIO $ atomicModifyIORef' ref $ \i -> (i+1, i+1)
-              text ("Hello " <> name <> ", you are visitor number " <> T.pack (show visitorNumber))
+app :: SpockM () () () ()
+app = do
+  middleware (staticPolicy (addBase "static"))
+  get root homePage
+  post root $ do
+    date' <- param "date"
+    cost' <- param "cost"
+    case (date', cost') of
+      (Just date, Just cost) -> do
+        _ <- liftIO $ indexDoc date cost
+        pure ()
+      _ -> pure ()
+    homePage
+  where homePage = html . toStrict $ R.renderHtml home
+
+indexDoc :: Text -> Text -> IO Reply
+indexDoc timestamp cost = do
+  withBH defaultManagerSettings es $
+    bulk $ V.fromList
+      [ BulkIndexAuto (IndexName "gas") (MappingName "_doc") $
+        object [ "timestamp" .= timestamp
+               , "price" .= cost
+               ]
+      ]
+
+home :: H.Html
+home = H.docTypeHtml $ do
+  H.head $ do
+    H.title application
+    H.link ! rel "stylesheet" ! type_ "text/css" ! href "/style.css"
+  H.body $ do
+    H.h1 application
+    H.form ! action "/" ! method "POST" $ do
+      H.label "Purchase Date" ! for "date"
+      H.input ! type_ "date" ! name "date" ! Text.Blaze.Html5.Attributes.id "date"
+
+      H.label "Cost" ! for "cost"
+      H.input ! type_ "number" ! name "cost"
+        ! Text.Blaze.Html5.Attributes.id "cost"
+        ! step "0.01"
+
+      H.input ! type_ "submit" ! value "Save"
